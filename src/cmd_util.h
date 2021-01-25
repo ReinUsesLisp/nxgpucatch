77084a46b4cc69df15f0b7f91eea7c77952c3137
ReinUsesLisp <reinuseslisp@airmail.cc>
2021-01-25 06:18:31 -0300

    cmd_util: Throw when queue is in error state

--- src/cmd_util.h
+++ src/cmd_util.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdexcept>
+
 #include <deko3d.hpp>
 
 #include "device.h"
@@ -7,6 +9,9 @@
 template <typename Func>
 void RecordRunWait(Func&& func) {
     static constexpr size_t MAX_SIZE{64 * 1024};
+    if (queue.isInErrorState()) {
+        throw std::runtime_error{"Queue in error state"};
+    }
     dk::UniqueCmdBuf cmdbuf{dk::CmdBufMaker{device}.create()};
     dk::UniqueMemBlock heap{dk::MemBlockMaker{device, MAX_SIZE}.create()};
     cmdbuf.addMemory(heap, 0, MAX_SIZE);
@@ -14,4 +19,7 @@ void RecordRunWait(Func&& func) {
     queue.submitCommands(cmdbuf.finishList());
     queue.flush();
     queue.waitIdle();
+    if (queue.isInErrorState()) {
+        throw std::runtime_error{"Queue in error state"};
+    }
 }
