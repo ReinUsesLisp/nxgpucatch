8154a00799915791515a318f67df1c5165eca2a1
Rodrigo Locatti <rodrigo.locatti@gmail.com>
2021-03-03 18:59:34 -0300

    Add HFMA2 tests

--- src/fp16_run.h
+++ src/fp16_run.h
@@ -22,6 +22,21 @@ inline u32 Run(u32 lhs, u32 rhs, std::string code, f16x2 base = {}) {
                          std::array{BitCast<u32>(base), lhs, rhs});
 }
 
+inline u32 Run(u32 a, u32 b, u32 c, std::string code, f16x2 base = {}) {
+    return EvalUtil::Run(".dksh compute\n"
+                         "main:\n"
+                         "MOV R0, c[0x0][0x140];\n"
+                         "MOV R1, c[0x0][0x144];\n"
+                         "MOV R2, c[2][0];"
+                         "MOV R3, c[2][4];"
+                         "MOV R4, c[2][8];"
+                         "MOV R5, c[2][12];" +
+                             code +
+                             "STG.E [R0], R2;\n"
+                             "EXIT;\n",
+                         std::array{BitCast<u32>(base), a, b, c});
+}
+
 inline f16x2 RunF16x2(float lhs, float rhs, std::string code, f16x2 base = {}) {
     return BitCast<f16x2>(Run(BitCast<u32>(lhs), BitCast<u32>(rhs), std::move(code), base));
 }
@@ -52,6 +67,86 @@ inline float RunF32(float lhs, f16x2 rhs, std::string code, f16x2 base = {}) {
 
 inline float RunF32(f16x2 lhs, f16x2 rhs, std::string code, f16x2 base = {}) {
     return BitCast<float>(Run(BitCast<u32>(lhs), BitCast<u32>(rhs), std::move(code), base));
+}
+
+inline f16x2 RunF16x2(float a, float b, float c, std::string code, f16x2 base = {}) {
+    return BitCast<f16x2>(
+        Run(BitCast<u32>(a), BitCast<u32>(b), BitCast<u32>(c), std::move(code), base));
+}
+
+inline f16x2 RunF16x2(f16x2 a, float b, float c, std::string code, f16x2 base = {}) {
+    return BitCast<f16x2>(
+        Run(BitCast<u32>(a), BitCast<u32>(b), BitCast<u32>(c), std::move(code), base));
+}
+
+inline f16x2 RunF16x2(float a, f16x2 b, float c, std::string code, f16x2 base = {}) {
+    return BitCast<f16x2>(
+        Run(BitCast<u32>(a), BitCast<u32>(b), BitCast<u32>(c), std::move(code), base));
+}
+
+inline f16x2 RunF16x2(f16x2 a, f16x2 b, float c, std::string code, f16x2 base = {}) {
+    return BitCast<f16x2>(
+        Run(BitCast<u32>(a), BitCast<u32>(b), BitCast<u32>(c), std::move(code), base));
+}
+
+inline f16x2 RunF16x2(float a, float b, f16x2 c, std::string code, f16x2 base = {}) {
+    return BitCast<f16x2>(
+        Run(BitCast<u32>(a), BitCast<u32>(b), BitCast<u32>(c), std::move(code), base));
+}
+
+inline f16x2 RunF16x2(f16x2 a, float b, f16x2 c, std::string code, f16x2 base = {}) {
+    return BitCast<f16x2>(
+        Run(BitCast<u32>(a), BitCast<u32>(b), BitCast<u32>(c), std::move(code), base));
+}
+
+inline f16x2 RunF16x2(float a, f16x2 b, f16x2 c, std::string code, f16x2 base = {}) {
+    return BitCast<f16x2>(
+        Run(BitCast<u32>(a), BitCast<u32>(b), BitCast<u32>(c), std::move(code), base));
+}
+
+inline f16x2 RunF16x2(f16x2 a, f16x2 b, f16x2 c, std::string code, f16x2 base = {}) {
+    return BitCast<f16x2>(
+        Run(BitCast<u32>(a), BitCast<u32>(b), BitCast<u32>(c), std::move(code), base));
+}
+
+inline float RunF32(float a, float b, float c, std::string code, f16x2 base = {}) {
+    return BitCast<float>(
+        Run(BitCast<u32>(a), BitCast<u32>(b), BitCast<u32>(c), std::move(code), base));
+}
+
+inline float RunF32(f16x2 a, float b, float c, std::string code, f16x2 base = {}) {
+    return BitCast<float>(
+        Run(BitCast<u32>(a), BitCast<u32>(b), BitCast<u32>(c), std::move(code), base));
+}
+
+inline float RunF32(float a, f16x2 b, float c, std::string code, f16x2 base = {}) {
+    return BitCast<float>(
+        Run(BitCast<u32>(a), BitCast<u32>(b), BitCast<u32>(c), std::move(code), base));
+}
+
+inline float RunF32(f16x2 a, f16x2 b, float c, std::string code, f16x2 base = {}) {
+    return BitCast<float>(
+        Run(BitCast<u32>(a), BitCast<u32>(b), BitCast<u32>(c), std::move(code), base));
+}
+
+inline float RunF32(float a, float b, f16x2 c, std::string code, f16x2 base = {}) {
+    return BitCast<float>(
+        Run(BitCast<u32>(a), BitCast<u32>(b), BitCast<u32>(c), std::move(code), base));
+}
+
+inline float RunF32(f16x2 a, float b, f16x2 c, std::string code, f16x2 base = {}) {
+    return BitCast<float>(
+        Run(BitCast<u32>(a), BitCast<u32>(b), BitCast<u32>(c), std::move(code), base));
+}
+
+inline float RunF32(float a, f16x2 b, f16x2 c, std::string code, f16x2 base = {}) {
+    return BitCast<float>(
+        Run(BitCast<u32>(a), BitCast<u32>(b), BitCast<u32>(c), std::move(code), base));
+}
+
+inline float RunF32(f16x2 a, f16x2 b, f16x2 c, std::string code, f16x2 base = {}) {
+    return BitCast<float>(
+        Run(BitCast<u32>(a), BitCast<u32>(b), BitCast<u32>(c), std::move(code), base));
 }
 
 } // namespace Fp16Run
---
+++ src/tests/shader/hfma2.cpp
@@ -0,0 +1,111 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "fp16_run.h"
+
+using namespace Fp16Run;
+
+using V = std::array<float, 2>;
+[[maybe_unused]] inline V X(f16x2 v) {
+    return {v[0], v[1]};
+}
+
+TEST_CASE("HFMA2 Simple", "[shader]") {
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, {1, 2}, "HFMA2.FTZ R2, R3, R4, R5;") == f16x2{3, 4});
+
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, {1, 1}, "HFMA2.FTZ R2, R3, R4, R5;") == f16x2{3, 3});
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, {1, 1}, "HFMA2.FTZ R2, R3, -R4, R5;") == f16x2{-1, -1});
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, {1, 1}, "HFMA2.FTZ R2, R3, R4, -R5;") == f16x2{1, 1});
+
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, {1, 1}, "HFMA2.FTZ R2, R3.H0_H0, R4, R5;") == f16x2{3, 2});
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, {1, 1}, "HFMA2.FTZ R2, R3.H1_H1, R4, R5;") == f16x2{5, 3});
+    REQUIRE(RunF16x2(1, {2, 1}, {1, 1}, "HFMA2.FTZ R2, R3.F32, R4, R5;") == f16x2{3, 2});
+
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, {1, 1}, "HFMA2.FTZ R2, R3, R4.H0_H0, R5;") == f16x2{3, 5});
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, {1, 1}, "HFMA2.FTZ R2, R3, R4.H1_H1, R5;") == f16x2{2, 3});
+    REQUIRE(RunF16x2({1, 2}, 1, {1, 1}, "HFMA2.FTZ R2, R3, R4.F32, R5;") == f16x2{2, 3});
+
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, {1, 2}, "HFMA2.FTZ R2, R3, R4, R5.H0_H0;") == f16x2{3, 3});
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, {2, 1}, "HFMA2.FTZ R2, R3, R4, R5.H1_H1;") == f16x2{3, 3});
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, 1, "HFMA2.FTZ R2, R3, R4, R5.F32;") == f16x2{3, 3});
+
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, {1, 1}, "HFMA2.MRG_H0.FTZ R2, R3, R4, R5;", {0, 0}) ==
+            f16x2{3, 0});
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, {1, 1}, "HFMA2.MRG_H1.FTZ R2, R3, R4, R5;", {0, 0}) ==
+            f16x2{0, 3});
+    REQUIRE(RunF32({1, 2}, {2, 1}, {1, 1}, "HFMA2.F32.FTZ R2, R3, R4, R5;", {0, 0}) == 3);
+    REQUIRE(RunF32({1, 2}, {2, 1}, {1, 1}, "HFMA2.F32.FTZ.SAT R2, R3, R4, R5;", {0, 0}) == 1);
+
+    REQUIRE(RunF16x2({1, 1}, 1, {1, 1}, "HFMA2.FTZ R2, R3, c[2][8], R5;") == f16x2{2, 2});
+    REQUIRE(RunF16x2({1, 1}, 1, {1, 1}, "HFMA2.FTZ R2, R3, -c[2][8], R5;") == f16x2{0, 0});
+    REQUIRE(RunF16x2({1, 2}, 1, {1, 1}, "HFMA2.FTZ R2, R3, c[2][8], -R5;") == f16x2{0, 1});
+
+    REQUIRE(RunF16x2({1, 2}, 1, {1, 1}, "HFMA2.FTZ R2, R3.H0_H0, c[2][8], R5;") == f16x2{2, 2});
+    REQUIRE(RunF16x2({1, 2}, 1, {1, 1}, "HFMA2.FTZ R2, R3.H1_H1, c[2][8], R5;") == f16x2{3, 3});
+    REQUIRE(RunF16x2(1, 1, {1, 1}, "HFMA2.FTZ R2, R3.F32, c[2][8], R5;") == f16x2{2, 2});
+
+    REQUIRE(RunF16x2({1, 2}, 1, {1, 1}, "HFMA2.FTZ R2, R3, c[2][8], R5;") == f16x2{2, 3});
+
+    REQUIRE(RunF16x2({1, 2}, 1, {1, 2}, "HFMA2.FTZ R2, R3, c[2][8], R5.H0_H0;") == f16x2{2, 3});
+    REQUIRE(RunF16x2({1, 2}, 1, {2, 1}, "HFMA2.FTZ R2, R3, c[2][8], R5.H1_H1;") == f16x2{2, 3});
+    REQUIRE(RunF16x2({1, 2}, 1, 1, "HFMA2.FTZ R2, R3, c[2][8], R5.F32;") == f16x2{2, 3});
+
+    REQUIRE(RunF16x2({1, 2}, 1, {1, 1}, "HFMA2.MRG_H0.FTZ R2, R3, c[2][8], R5;", {0, 0}) ==
+            f16x2{2, 0});
+    REQUIRE(RunF16x2({1, 2}, 1, {1, 1}, "HFMA2.MRG_H1.FTZ R2, R3, c[2][8], R5;", {0, 0}) ==
+            f16x2{0, 3});
+    REQUIRE(RunF32({1, 2}, 1, {1, 1}, "HFMA2.F32.FTZ R2, R3, c[2][8], R5;", {0, 0}) == 2);
+    REQUIRE(RunF32({1, 2}, 1, {1, 1}, "HFMA2.F32.FTZ.SAT R2, R3, c[2][8], R5;", {0, 0}) == 1);
+
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, 1, "HFMA2.FTZ R2, R3, R4, c[2][12];") == f16x2{3, 3});
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, 1, "HFMA2.FTZ R2, R3, -R4, c[2][12];") == f16x2{-1, -1});
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, 1, "HFMA2.FTZ R2, R3, R4, -c[2][12];") == f16x2{1, 1});
+
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, 1, "HFMA2.FTZ R2, R3.H0_H0, R4, c[2][12];") == f16x2{3, 2});
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, 1, "HFMA2.FTZ R2, R3.H1_H1, R4, c[2][12];") == f16x2{5, 3});
+    REQUIRE(RunF16x2(1, {2, 1}, 1, "HFMA2.FTZ R2, R3.F32, R4, c[2][12];") == f16x2{3, 2});
+
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, 1, "HFMA2.FTZ R2, R3, R4.H0_H0, c[2][12];") == f16x2{3, 5});
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, 1, "HFMA2.FTZ R2, R3, R4.H1_H1, c[2][12];") == f16x2{2, 3});
+    REQUIRE(RunF16x2({1, 2}, 1, 1, "HFMA2.FTZ R2, R3, R4.F32, c[2][12];") == f16x2{2, 3});
+
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, 1, "HFMA2.MRG_H0.FTZ R2, R3, R4, c[2][12];", {0, 0}) ==
+            f16x2{3, 0});
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, 1, "HFMA2.MRG_H1.FTZ R2, R3, R4, c[2][12];", {0, 0}) ==
+            f16x2{0, 3});
+    REQUIRE(RunF32({1, 2}, {2, 1}, 1, "HFMA2.F32.FTZ R2, R3, R4, c[2][12];", {0, 0}) == 3);
+    REQUIRE(RunF32({1, 2}, {2, 1}, 1, "HFMA2.F32.FTZ.SAT R2, R3, R4, c[2][12];", {0, 0}) == 1);
+
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, {1, 1}, "HFMA2.FTZ R2, R3, 1, 2, R5;") == f16x2{3, 3});
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, {1, 1}, "HFMA2.FTZ R2, R3, -1, -2, R5;") == f16x2{-1, -1});
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, {1, 1}, "HFMA2.FTZ R2, R3, 1, 2, -R5;") == f16x2{1, 1});
+
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, {1, 1}, "HFMA2.FTZ R2, R3.H0_H0, 1, 2, R5;") == f16x2{3, 2});
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, {1, 1}, "HFMA2.FTZ R2, R3.H1_H1, 1, 2, R5;") == f16x2{5, 3});
+    REQUIRE(RunF16x2(1, {2, 1}, {1, 1}, "HFMA2.FTZ R2, R3.F32, 1, 2, R5;") == f16x2{3, 2});
+
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, {1, 2}, "HFMA2.FTZ R2, R3, 1, 2, R5.H0_H0;") == f16x2{3, 3});
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, {2, 1}, "HFMA2.FTZ R2, R3, 1, 2, R5.H1_H1;") == f16x2{3, 3});
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, 1, "HFMA2.FTZ R2, R3, 1, 2, R5.F32;") == f16x2{3, 3});
+
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, {1, 1}, "HFMA2.MRG_H0.FTZ R2, R3, 1, 2, R5;", {0, 0}) ==
+            f16x2{3, 0});
+    REQUIRE(RunF16x2({1, 2}, {2, 1}, {1, 1}, "HFMA2.MRG_H1.FTZ R2, R3, 1, 2, R5;", {0, 0}) ==
+            f16x2{0, 3});
+    REQUIRE(RunF32({1, 2}, {2, 1}, {1, 1}, "HFMA2.F32.FTZ R2, R3, 1, 2, R5;", {0, 0}) == 3);
+    REQUIRE(RunF32({1, 2}, {2, 1}, {1, 1}, "HFMA2.F32.FTZ.SAT R2, R3, 1, 2, R5;", {0, 0}) == 1);
+
+    REQUIRE(X(RunF16x2({1, 2}, 0, 0, "HFMA2_32I.FTZ R2, R3, 1, 2, R2;", {1, 2})) == V{3, 4});
+    REQUIRE(X(RunF16x2({1, 2}, 0, 0, "HFMA2_32I.FTZ R2, R3, 1, 2, -R2;", {1, 2})) == V{1, 0});
+    REQUIRE(X(RunF16x2({1, 2}, 0, 0, "HFMA2_32I.FTZ R2, R3.H0_H0, 1, 2, R2;", {1, 2})) == V{3, 3});
+    REQUIRE(X(RunF16x2({1, 2}, 0, 0, "HFMA2_32I.FTZ R2, R3.H1_H1, 1, 2, R2;", {1, 2})) == V{5, 4});
+    REQUIRE(X(RunF16x2(3, 0, 0, "HFMA2_32I.FTZ R2, R3.F32, 1, 2, R2;", {1, 2})) == V{7, 5});
+}
+
+TEST_CASE("HFMA2 Promotions", "[shader]") {
+    REQUIRE(RunF16x2(1, {2, 1}, 1, "HFMA2.FTZ R2, R3.F32, R4, R5.F32;") == f16x2{3, 2});
+    REQUIRE(RunF16x2(1, 1, {1, 2}, "HFMA2.FTZ R2, R3.F32, R4.F32, R5;") == f16x2{2, 3});
+    REQUIRE(RunF16x2(1, 1, 1, "HFMA2.FTZ R2, R3.F32, R4.F32, R5.F32;") == f16x2{2, 2});
+    REQUIRE(RunF32(1, 1, 2, "HFMA2.F32.FTZ R2, R3.F32, R4.F32, R5.F32;") == 3);
+    REQUIRE(RunF32(1, 1, 2, "HFMA2.F32.FTZ.SAT R2, R3.F32, R4.F32, R5.F32;") == 1);
+    REQUIRE(RunF32(1, 1, 2, "HFMA2.F32.FTZ.SAT R2, R3.F32, -R4.F32, -R5.F32;") == 0);
+}
